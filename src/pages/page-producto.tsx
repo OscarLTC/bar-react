b97cbbb949395e16e79d38bf367fd01bcc852ad8
Oscar <oscartaco2003@gmail.com>
2022-06-27 11:14:37 -0500

    fix hover nav

--- src/pages/page-producto.tsx
+++ src/pages/page-producto.tsx
@@ -1,7 +1,4 @@
-import { Route, Routes } from "react-router-dom";
 import { Catalago } from "../components/producto-component/catalogo";
-import { CatalagoSkeleton } from "../components/producto-component/catalogo/skeleton";
-import { Detalle } from "../components/producto-component/detalle/detalle";
 import { Header } from "../components/producto-component/header";
 
 export const PageProducto = () => {
